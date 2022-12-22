0d05bcd9f9f1fd6072c4c4d6d488a2d0a9c2e770
Joshua Uzzell <josh.uzzell@gmail.com>
2022-12-21 22:59:26 -0500

    Configure Webpack to support multiple HTML pages via HtmlWebpackPlugin, and turn on watch feature

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, 'src/app/pages/home/index.js'), 
+    reports: path.resolve(__dirname, 'src/app/pages/reports/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name]_[contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+    publicPath: '/'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [],
+  },
+  watch: true, 
+  watchOptions: {
+    aggregateTimeout: 200,
+    poll: 1000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Monthly Budget Tracker',
+      filename: 'index.html',
+      template: 'src/app/pages/home/index.html',
+      chunks: [ 'index' ]
+    }), 
+    new HtmlWebpackPlugin({
+      title: 'Budget Reports',
+      filename: 'reports.html',
+      template: 'src/app/pages/reports/index.html', 
+      chunks: [ 'reports' ]
+    })
+  ],
+}
