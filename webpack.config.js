eba99832d02bf79134a1d45aff47a90adda74c28
Joshua Uzzell <josh.uzzell@gmail.com>
2023-01-10 18:48:05 -0500

    - Add support for production and development environments - Add deployment feature

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
+const fs = require('fs');
 const path = require('path');
-var fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { ContextExclusionPlugin } = require('webpack');
+const config = require( './deploy/config.json' );
  
 const htmlFileRegex = new RegExp(/(src\/app\/pages\/)|(\/index.html)/, 'ig');
 let htmlFiles = [];
@@ -24,44 +24,53 @@ htmlFiles.map(file => {
   }
 ); 
 
-module.exports = {
-  mode: 'development',
-  entry: entryPoints,
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: '[name]_[contenthash].js',
-    clean: false,
-    assetModuleFilename: '[name][ext]',
-    publicPath: '/'
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.scss|.css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },  
-      {
-        test: /\.(png|svg|jpg|gif|jpe?g)$/,  
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[name][ext]'
+module.exports = ( env ) => {
+
+  const publicPath = ( env.production ) ? config.targets.prod.publicPath : config.targets.dev.publicPath; 
+  const assetModuleFilename = ( env.production ) ? config.targets.prod.assetModuleFilename : config.targets.dev.assetModuleFilename; 
+  const watch = ( env.production ) ? false : true;  
+  const clean = ( env.production ) ? true : false;  
+
+  return {
+    mode: 'development',
+    entry: entryPoints,
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: '[name]_[contenthash].js',
+      clean: clean,
+      assetModuleFilename: assetModuleFilename + './[name][ext]',
+      publicPath: publicPath + '/'
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.scss|.css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },  
+        {
+          test: /\.(png|svg|jpg|gif|jpe?g)$/,  
+          type: 'asset/resource',
+          generator: {
+            filename: 'images/[name][ext]'
+          }
         }
-      }
-    ],
-  },
-  watch: true, 
-  watchOptions: {
-    aggregateTimeout: 200,
-    poll: 1000,
-  },
-  plugins: [
-    ...htmlFiles.map(htmlFile =>
-      new HtmlWebpackPlugin({
-        template: htmlFile,
-        filename: htmlFile.replace( htmlFileRegex, "" ) + ".html", 
-        chunks: [ htmlFile.replace( htmlFileRegex, "" ) ]
-      })
-  )
-  ],
+      ],
+    },
+    watch: watch, 
+    watchOptions: {
+      aggregateTimeout: 200,
+      poll: 1000,
+    },
+    plugins: [
+      ...htmlFiles.map(htmlFile =>
+        new HtmlWebpackPlugin({
+          template: htmlFile,
+          filename: htmlFile.replace( htmlFileRegex, "" ) + ".html", 
+          chunks: [ htmlFile.replace( htmlFileRegex, "" ) ]
+        })
+    )
+    ]
+  }
+
 }
